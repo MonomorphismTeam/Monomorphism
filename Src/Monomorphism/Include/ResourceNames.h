a2f755f8ad3295647330776affc18cc4530f9192
AirGuanZ <AirGuanZ@gmail.com>
2017-10-26 16:44:52 +0800

    资源路径注册文件

---
+++ Src/Monomorphism/Include/ResourceNames.h
@@ -0,0 +1,19 @@
+/*================================================================
+Filename: ResourceNames.h
+Date: 2017.10.26
+Created by AirGuanZ
+================================================================*/
+
+/*
+    ×ÊÔ´Â·¾¶×¢²á
+*/
+
+#ifndef __RESOURCE_NAMES_H__
+#define __RESOURCE_NAMES_H__
+
+#define MM_FILENAME(X) _MM_FILENAME(X)
+#define _MM_FILENAME(X) "Bin\\"##X
+
+#define ACTOR_STANDING_ANIMATION_CONFIGURE MM_FILENAME("Actor\\Standing.cfg")
+
+#endif //__RESOURCE_NAMES_H__
