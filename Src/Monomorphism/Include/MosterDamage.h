fa596fcad8a7c4369d3d4f5fb6756b04ff9745e1
CCH <609726916@qq.com>
2017-11-10 21:02:09 +0800

    done mainmenu
    main menu

---
+++ Src/Monomorphism/Include/MosterDamage.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<vector>
+#include<OWE.h>
+#include "DamageArea.h"
+#include <string>
+
+#define __MONSTERDAMAGE_H__
+class MonsterDamage : DamageArea
+{
+public:
+	MonsterDamage();
+	MonsterDamage();
+	//void ~DamageArea();
+
+private:
+
+};
